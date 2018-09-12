21eecb1e428f9fd44bec5f255b12a848da626f8f
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-12 03:35:53 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,14 +15,7 @@ def first_challenge
 
   #your code here
   
-  contacts.each do | attribute, value |
-    if attribute ===
-      :favorite_icecream_flavors
-      value.each do |flavor|
-        flavor.delete_if("strawberry")
-      end
-    end
-  end
+ 
 
 
   #remember to return your newly altered contacts hash!
